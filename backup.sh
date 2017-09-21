70616eb17ab975a6afc5c859efed952570c806b8
User <sciner@users.noreply.github.com>
2017-09-21 14:51:07 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -173,25 +173,27 @@ function backupMongoDb() {
 
 }
 
-log "start\n"
+# USAGE EXAMPLE:
 
-backupDir apache2 /etc/apache2
-backupDir nginx /etc/nginx
-backupDir network /etc/network
-backupDir php5 /etc/php5
-
-backupMongoDb 127.0.0.1
-
-# Store all DB to separated archive
-backupPgsqlRecv
-
-# Store all directories to separated archive
-backupDirRecv /var/www
-
-# backupMysql  $mysql_host $mysql_user $mysql_pass --all_databases
-backupMysql  $mysql_host $mysql_user $mysql_pass sales
-backupMysql  $mysql_host $mysql_user $mysql_pass mysql
-backupMysql  $mysql_host $mysql_user $mysql_pass redmine
-backupMysql  $mysql_host $mysql_user $mysql_pass information_schema
-
-sendLog
+# log "start\n"
+#
+# backupDir apache2 /etc/apache2
+# backupDir nginx /etc/nginx
+# backupDir network /etc/network
+# backupDir php5 /etc/php5
+#
+# backupMongoDb 127.0.0.1
+#
+# # Store all DB to separated archive
+# backupPgsqlRecv
+#
+# # Store all directories to separated archive
+# backupDirRecv /var/www
+#
+# # backupMysql  $mysql_host $mysql_user $mysql_pass --all_databases
+# backupMysql  $mysql_host $mysql_user $mysql_pass sales
+# backupMysql  $mysql_host $mysql_user $mysql_pass mysql
+# backupMysql  $mysql_host $mysql_user $mysql_pass redmine
+# backupMysql  $mysql_host $mysql_user $mysql_pass information_schema
+#
+# sendLog
